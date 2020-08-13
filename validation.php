06bd535e11325000fe9cb3ddde7cac524e38e2d4
Damien Mota <mota.damien@gmail.com>
2020-08-13 17:47:56 -0500

    Adding a function to send sms using PHP

---
+++ validation.php
@@ -0,0 +1,11 @@
+<?php
+$conn = mysqli_connect('localhost', 'damien', 'Oimadi*1', 'application_management');
+if(!$conn)
+{
+	echo 'Connection ERRROR';
+}
+else
+{
+	echo file_get_contents("validate_code.html");
+}
+?>
