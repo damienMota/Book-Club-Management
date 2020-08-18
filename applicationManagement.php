624ab24b7511f5276fca5ca03de8cecdacabb0e5
Damien Mota <mota.damien@gmail.com>
2020-08-18 15:25:42 -0500

    Prepared Statments working properly, next step is to build a function that can read and match a verification code provided in the email.

--- applicationManagement.php
+++ applicationManagement.php
@@ -107,15 +107,35 @@
 		}
 	}
 	if($_POST["action"] == "initiate_application")
-	{
+	{	
+		//Building Prepared Statement for Insert into table application_main//
+		$insertName = mysqli_real_escape_string($conn, $name);
+		$insertPrimaryPhone = mysqli_real_escape_string($conn, $_POST["primary_phone_number"]);
+		$insertPrimaryEmail = mysqli_real_escape_string($conn, $_POST["primary_email"]);
+		$insertApplicationStatus = mysqli_real_escape_string($conn, "pending");
+		$insertValidationCode = mysqli_real_escape_string($conn, $verification_code);
+		
+		$sql = "INSERT INTO application_main (name,primary_phone_number,primary_email,application_status,validation_code)
+				VALUES (?, ?, ?, ?, ?);";
+		$stmt = mysqli_stmt_init($conn);
+		if(!mysqli_stmt_prepare($stmt, $sql))
+		{
+			echo "SQL ERROR";
+		}
+		else
+		{
+			mysqli_stmt_bind_param($stmt, "ssssi", $insertName, $insertPrimaryPhone, $insertPrimaryEmail, $insertApplicationStatus, $insertValidationCode);
+			mysqli_stmt_execute($stmt);
+		}
+		
+		//Formatting for PHP Mailer//
 		$name = $_POST["first_name"].' '.$_POST["last_name"];
 		$email = $_POST["primary_email"];
 		$subject = 'Test Subject';
 		$verification_code = mt_rand(100000, 999999);
 		$fixedTemplate = str_replace("[verification_code]",$verification_code,file_get_contents("initiation_email.html"));
 		$body = $fixedTemplate;
-
-		"INSERT INTO application_main (name,primary_phone_number,primary_email,application_status,business_name,reference_number,validate_token) VALUES ('$1', '3238069526', 'outbyaspen@gmail.com', 'pending', '', '', '123333')",array($name);
+		
 		require_once "PHPMailer/PHPMailer.php";
 		require_once "PHPMailer/SMTP.php";
 		require_once "PHPMailer/Exception.php";
